d41c269809176038d9db93dbc356a1719972237d
Marcus <marcus@iconexlog.com.br>
2021-02-25 21:37:31 -0300

    transferindo projeto para o next.js

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,13 @@
+
+export function ExperienteBar(){
+    return(
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '75%'}} />
+                <span style={{left: '75%'}} >450 xp</span>
+            </div>
+            <span className="current-experience">600 xp</span>
+        </header>
+    )    
+}
